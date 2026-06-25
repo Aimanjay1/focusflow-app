5288328a65737d616a0e46a89ec9eb109a531f16
aimanjay1 <aimanjay1@gmail.com>
2026-06-25 21:58:00 +0800

    go

--- components/ChartData.tsx
+++ components/ChartData.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Bar, BarChart, CartesianGrid } from "recharts";
-import { ChartContainer, type ChartConfig } from "@/components/ui/chart";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { ChartContainer, ChartTooltip, ChartTooltipContent, type ChartConfig } from "@/components/ui/chart";
 
 const chartWeeklyData = [
   { day: "Mon", todo: 30, doing: 20, done: 10 },
@@ -32,6 +32,8 @@ export function ChartData() {
     <ChartContainer config={chartConfig} className="min-h-50 w-full">
       <BarChart accessibilityLayer data={chartWeeklyData}>
         <CartesianGrid vertical={false} />
+        <XAxis dataKey="day" />
+        <ChartTooltip content={<ChartTooltipContent />} />
         <Bar dataKey="todo" fill="var(--color-todo)" radius={4} />
         <Bar dataKey="doing" fill="var(--color-doing)" radius={4} />
         <Bar dataKey="done" fill="var(--color-done)" radius={4} />
